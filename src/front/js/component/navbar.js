2fec3c50402ee14a2ecff5c40f4252d5f1f3b7ad
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2024-03-01 16:37:18 +0000

    useEffects and useStates in navbar functional without suggestions, started working on profile view

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+  const { store, actions } = useContext(Context);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    if (!searchTerm) {
+      setSuggestions([]);
+    }
+  }, [searchTerm]);
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+    setSuggestions(actions.getSuggestions(e.target.value))
+  };
+
+
   return (
     <div className="position-relative">
       <nav className="navbar bg-dark bg-gradient justify-content-evenly flex-row">
         <div className="container-fluid ">
           <div className="d-flex flex-row w-50">
+            {/*---------------------------------------MENU ICON---------------------------------*/}
             <button
               className="btn"
               type="button"
@@ -15,9 +33,9 @@ export const Navbar = () => {
               aria-expanded="false"
               aria-controls="navbar-menu"
             >
-              <i className="fa-solid fa-bars" style={{color: "#992899"}}></i>
+              <i className="fa-solid fa-bars" style={{ color: "#992899" }}></i>
             </button>
-
+            {/*---------------------------------------LOGO---------------------------------*/}
             <Link to={"/"} className="navbar-brand mx-3">
               <img
                 src="https://cdn.discordapp.com/attachments/1200818313421398017/1208775421672292382/allanrogerhaze_Create_an_impactful_and_memorable_logo_for_a_gam_4523fdda-cff8-4f99-8771-dca36d9acbfc.png?ex=65edbd56&is=65db4856&hm=240bb1292bef08ee14f51727418fd731fb104e9bbf8e759aa3b8d7ac273f81c5&"
@@ -26,38 +44,78 @@ export const Navbar = () => {
                 height="34"
               />
             </Link>
-
-            <form className="input-group mx-3 w-25">
-              <span className="input-group-text rounded-start-5" id="search-icon">
-                <i className="fa-solid fa-magnifying-glass" style={{color: "#992899"}}></i>
-              </span>
-              <input
-                type="text"
-                className="form-control rounded-end-5"
-                placeholder="Búsqueda"
-                aria-label="Búsqueda"
-                aria-describedby="search-icon"
-              ></input>
-            </form>
+            {/*---------------------------------------SEARCH BAR---------------------------------*/}
+            <div className="d-flex flex-row">
+              <p className="mx-2">
+                <button
+                  className="btn p-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseSearch"
+                  aria-expanded="false"
+                  aria-controls="collapseSearch"
+                >
+                  <i
+                    className="fa-solid fa-magnifying-glass"
+                    style={{ color: "#992899" }}
+                  ></i>
+                </button>
+              </p>
+              <div style={{ minHeight: "50px" }}>
+                <div
+                  className="collapse collapse-horizontal"
+                  id="collapseSearch"
+                >
+                  <form>
+                    <input
+                      type="search"
+                      className="form-control rounded-5 w-auto text-white bg-transparent"
+                      style={{ maxHeight: "30px" }}
+                      aria-label="Búsqueda"
+                      value={searchTerm}
+                      onChange={handleInputChange}
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
+          {/*---------------------------------------SIGNUP---------------------------------*/}
           <button className="mx-3 btn">
-            <i className="fa-solid fa-user-plus" style={{color: "#992899"}}></i>
+            <i
+              className="fa-solid fa-user-plus"
+              style={{ color: "#992899" }}
+            ></i>
           </button>
         </div>
       </nav>
+      {/*---------------------------------------MENU CONTENTS---------------------------------*/}
       <div
         className="collapse bg-dark bg-gradient text-white position-absolute"
         id="navbar-menu"
         style={{ width: "140px", height: "100%" }}
       >
         <div className="d-flex flex-column ms-4 my-3">
-          <Link className="text-white text-decoration-none mb-2" to={"/"}>Mi perfil</Link>
-          <Link className="text-white text-decoration-none mb-2" to={"/"}>Publicar oferta</Link>
-          <Link className="text-white text-decoration-none mb-2" to={"/"}>Juegos</Link>
-          <Link className="text-white text-decoration-none mb-2" to={"/"}>Ofertas</Link>
+          <Link
+            className="text-white text-decoration-none mb-2"
+            to={"/profile/:username"}
+          >
+            Mi perfil
+          </Link>
+          <Link className="text-white text-decoration-none mb-2" to={"/"}>
+            Publicar oferta
+          </Link>
+          <Link className="text-white text-decoration-none mb-2" to={"/"}>
+            Juegos
+          </Link>
+          <Link className="text-white text-decoration-none mb-2" to={"/"}>
+            Ofertas
+          </Link>
         </div>
         <div className="ms-4 mb-3">
-          <button className="btn text-white" style={{background: "#992899"}}>Login</button>
+          <button className="btn text-white" style={{ background: "#992899" }}>
+            Login
+          </button>
         </div>
       </div>
     </div>
--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,11 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
 import { Context } from "../store/appContext";
 
 export const Profile = () => {
   const { store, actions } = useContext(Context);
+  const [username, setUsername] = useState("@Pere");
+  const [email, setEmail] = useState("pereayats@email.com");
+  const [steamUsername, setSteamUsername] = useState("");
+  const [twitchUsername, setTwitchUsername] = useState("");
 
   return (
     <div>
+      {/*---------------------------------------PROFILE DETAILS---------------------------------*/}
       <div className="row justify-content-center mt-3">
         <div className="d-flex flex-column text-white col-8">
           <h3>Mi perfil</h3>
@@ -14,79 +21,155 @@ export const Profile = () => {
               className="rounded-circle img-fluid m-5"
               src="https://scontent.xx.fbcdn.net/v/t1.15752-9/429857093_437082848988064_3333411511087179117_n.png?_nc_cat=108&ccb=1-7&_nc_sid=510075&_nc_ohc=oB8UaK6zJUcAX8DG7Pj&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRI6WYkb2BOpkY2dEyd2AVYZqf9FUYaKcFlToeIgOBLiQ&oe=66087116"
             />
-            <ul className="list-group my-5">
-              <div style={{ fontSize: "12px", color: "#992899" }}>Username</div>
-              <li className="list-group-item rounded-5 my-2 text-white bg-transparent">
-                <div className="me-auto">
-                  <p className="fw-bold mb-0">@Pere</p>
+            <form>
+              <ul className="list-group my-5">
+                {/*---------------------------------------Username---------------------------------*/}
+                <div style={{ fontSize: "12px", color: "#992899" }}>
+                  Usuario
                 </div>
-              </li>
-              <div style={{ fontSize: "12px", color: "#992899" }}>Email</div>
-              <li className="list-group-item rounded-5 my-2 text-white bg-transparent d-flex flex-row">
-                <div className="me-auto">
-                  <p className="fw-bold mb-0">pereayats@email.com</p>
-                </div>
-                <div className="ms-3">
-                  <button className="btn py-0">
-                    <i
-                      className="fa-solid fa-pencil"
-                      style={{ color: "#992899" }}
-                    ></i>
-                  </button>
-                </div>
-              </li>
-              <div style={{ fontSize: "12px", color: "#992899" }}>Password</div>
-              <div className="d-flex flex-row">
-                <li className="list-group-item rounded-5 my-2 text-white bg-transparent">
+                <li className="list-group-item rounded-5 my-2 text-white bg-transparent d-flex flex-row">
                   <div className="me-auto">
-                    <p className="fw-bold mb-0">*********</p>
+                    <p className="fw-bold mb-0">{username}</p>
+                  </div>
+                  <div className="ms-3">
+                    <button className="btn py-0">
+                      <i
+                        className="fa-solid fa-pencil"
+                        style={{ color: "#992899" }}
+                      ></i>
+                    </button>
                   </div>
                 </li>
-                <button
-                  className="btn text-white rounded-5 ms-3"
-                  style={{ background: "#992899" }}
-                >
-                  Cambiar
-                </button>
-              </div>
-
-              <div style={{ fontSize: "12px", color: "#992899" }}>
-                Interests
-              </div>
-              <li className="list-group-item border-0 my-2 text-white bg-transparent d-flex flex-row">
-                <div className="d-flex flex-wrap gap-2">
-                  <p className="bg-transparent rounded-5 border-white border p-2">
-                    Single Player
-                  </p>
-                  <p className="bg-transparent rounded-5 border-white border p-2">
-                    Horror
-                  </p>
-                  <p className="bg-transparent rounded-5 border-white border p-2">
-                    Survival
-                  </p>
-                  <p className="bg-transparent rounded-5 border-white border p-2">
-                    Third Person
-                  </p>
-                  <p className="bg-transparent rounded-5 border-white border p-2">
-                    Action-Adventure
-                  </p>
-                  <p className="bg-transparent rounded-5 border-white border p-2">
-                    3D
-                  </p>
+                {/*---------------------------------------Email---------------------------------*/}
+                <div style={{ fontSize: "12px", color: "#992899" }}>Correo</div>
+                <li className="list-group-item rounded-5 my-2 text-white bg-transparent d-flex flex-row">
+                  <div className="me-auto">
+                    <p className="fw-bold mb-0">{email}</p>
+                  </div>
+                  <div className="ms-3">
+                    <button className="btn py-0">
+                      <i
+                        className="fa-solid fa-pencil"
+                        style={{ color: "#992899" }}
+                      ></i>
+                    </button>
+                  </div>
+                </li>
+                {/*---------------------------------------Password---------------------------------*/}
+                <div style={{ fontSize: "12px", color: "#992899" }}>
+                  Contraseña
                 </div>
-                <div className="ms-3">
-                  <button className="btn">
-                    <i
-                      className="fa-solid fa-pencil"
-                      style={{ color: "#992899" }}
-                    ></i>
+                <div className="d-flex flex-row">
+                  <li className="list-group-item rounded-5 my-2 text-white bg-transparent">
+                    <div className="me-auto">
+                      <p className="fw-bold mb-0">*********</p>
+                    </div>
+                  </li>
+                  <button
+                    className="btn text-white rounded-5 ms-3"
+                    style={{ background: "#992899" }}
+                  >
+                    Cambiar
                   </button>
                 </div>
-              </li>
-            </ul>
+                {/*---------------------------------------Platforms usernames---------------------------------*/}
+                <div style={{ fontSize: "12px", color: "#992899" }}>
+                  Vincular
+                </div>
+                <li className="list-group-item border-0 my-2 text-white bg-transparent d-flex flex-row">
+                  {/*---------------------------------------Steam---------------------------------*/}
+                  <p className="me-2">
+                    <button
+                      className="btn p-0"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseSteam"
+                      aria-expanded="false"
+                      aria-controls="collapseSteam"
+                    >
+                      <i className="fa-brands fa-steam text-white fs-3"></i>
+                    </button>
+                  </p>
+                  <div style={{ minHeight: "50px" }}>
+                    <div
+                      className="collapse collapse-horizontal"
+                      id="collapseSteam"
+                    >
+                      <input
+                        type="text"
+                        className="form-control rounded-5 text-black w-auto"
+                        style={{ maxHeight: "30px" }}
+                        onChange={(e) => setSteamUsername(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  {/*---------------------------------------Twitch---------------------------------*/}
+                  <p className="mx-2">
+                    <button
+                      className="btn p-0"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseTwitch"
+                      aria-expanded="false"
+                      aria-controls="collapseTwitch"
+                    >
+                      <i className="fa-brands fa-twitch text-white fs-3"></i>
+                    </button>
+                  </p>
+                  <div style={{ minHeight: "50px" }}>
+                    <div
+                      className="collapse collapse-horizontal"
+                      id="collapseTwitch"
+                    >
+                      <input
+                        type="text"
+                        className="form-control rounded-5 text-black w-auto"
+                        style={{ maxHeight: "30px" }}
+                        onChange={(e) => setTwitchUsername(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                </li>
+                {/*---------------------------------------Interests---------------------------------*/}
+                <div style={{ fontSize: "12px", color: "#992899" }}>
+                  Intereses
+                </div>
+                <li className="list-group-item border-0 my-2 text-white bg-transparent d-flex flex-row">
+                  <div className="d-flex flex-wrap gap-2">
+                    <p className="bg-transparent rounded-5 border-white border p-2">
+                      Single Player
+                    </p>
+                    <p className="bg-transparent rounded-5 border-white border p-2">
+                      Horror
+                    </p>
+                    <p className="bg-transparent rounded-5 border-white border p-2">
+                      Survival
+                    </p>
+                    <p className="bg-transparent rounded-5 border-white border p-2">
+                      Third Person
+                    </p>
+                    <p className="bg-transparent rounded-5 border-white border p-2">
+                      Action-Adventure
+                    </p>
+                    <p className="bg-transparent rounded-5 border-white border p-2">
+                      3D
+                    </p>
+                  </div>
+                  <div className="ms-3">
+                    <button className="btn">
+                      <i
+                        className="fa-solid fa-pencil"
+                        style={{ color: "#992899" }}
+                      ></i>
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </form>
           </div>
         </div>
       </div>
+      {/*---------------------------------------TABS---------------------------------*/}
       <div className="row mt-3">
         <nav className="d-flex justify-content-center">
           <div className="nav nav-tabs" id="nav-tab" role="tablist">
@@ -131,13 +214,14 @@ export const Profile = () => {
             </button>
           </div>
         </nav>
+        {/*---------------------------------------TABS CONTENT---------------------------------*/}
         <div className="tab-content text-white" id="nav-tabContent">
           <div
             className="tab-pane fade show active d-flex justify-content-center"
             id="nav-my-posts"
             role="tabpanel"
             aria-labelledby="nav-my-posts-tab"
-            tabindex="0"
+            tabIndex="0"
           >
             Grid with deal cards
           </div>
@@ -146,7 +230,7 @@ export const Profile = () => {
             id="nav-saved"
             role="tabpanel"
             aria-labelledby="nav-saved-tab"
-            tabindex="0"
+            tabIndex="0"
           >
             Grid with saved deals
           </div>
@@ -155,7 +239,7 @@ export const Profile = () => {
             id="nav-alerts"
             role="tabpanel"
             aria-labelledby="nav-alerts-tab"
-            tabindex="0"
+            tabIndex="0"
           >
             Grid with deal cards based on interests
           </div>
