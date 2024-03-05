d3ed5d3d6c51e37a36044c3539fc626053096331
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2024-03-05 18:10:05 +0000

    layout and navbar reviewed

--- src/front/js/component/dealCard.js
+++ src/front/js/component/dealCard.js
@@ -4,19 +4,18 @@ import { Link } from "react-router-dom";
 export const DealCard = () => {
     return (
 
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{ width: "18rem" }}>
             <img src="https://picsum.photos/200" className="card-img-top" alt="..." />
             <div className="card-body flex-row d-flex">
                 <div>
-                <h5 className="card-title">title</h5>
-                <p className="card-text">Digital/CD</p>
-                <p className="card-text">00000</p>
-                <Link className="btn btn-link" to="/post/:id">Detalles</Link>
+                    <h5 className="card-title">Title</h5>
+                    <p className="card-text">Digital/CD</p>
+                    <p className="card-text">00000</p>
+                    <Link className="btn btn-link" to="/post/:id">Details</Link>
                 </div>
                 <div className="ms-auto">
-                    <p>Raiting</p>
+                    <p>Rating</p>
                 </div>
-                
             </div>
         </div>
 
