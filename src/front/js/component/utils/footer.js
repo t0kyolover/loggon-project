651874fe39507c1dcda627aa3c9be56977e521fe
Ekaterina Chavan <144109471+t0kyolover@users.noreply.github.com>
2024-05-13 22:40:56 +0000

    merge prep

--- src/front/js/component/utils/footer.js
+++ src/front/js/component/utils/footer.js
@@ -3,11 +3,7 @@ import React from "react";
 export const Footer = () => (
   <footer
     className="footer pt-3 bg-gradient"
-<<<<<<< HEAD
     style={{ background: "#020D19", bottom: 0, width: "100%" }}
-=======
-    style={{ background: "#020D19", bottom: 0, width: "100%"}}
->>>>>>> e3a9f5c11c8c5ee51e10a36c742ff058ebf08b20
   >
     <p className= "text-center pb-3 text-white">
       Made by DKA <i className="fa-solid fa-skull-crossbones"></i>
