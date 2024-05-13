5e2c7a1c8855037e8e9cb75c5d771398af4cc0ff
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2024-05-13 21:49:17 +0000

    imgBannerDeal

--- src/front/js/component/deal/bannerDeal.js
+++ src/front/js/component/deal/bannerDeal.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import bn1 from "../../../img/bn1.png";
+import "../../../styles/styles.css";
 
 
 export const BannerDeal = () => (
 
 <div className="wrapper">
         <div className="containerBanner">
-            <input type="radio" name="slide" id="c1" checked />
-            <label for="c1" className="cardBanner">
-                <div class="row">
+            <input type="radio" name="slide" id="c1" checked/>
+            <label for="c1" className="cardBanner card-effect" style={{ backgroundImage: `url(${bn1})` }}>
+                <div class="row ">
                     <div className="icon">1</div>
                     <div className="description">
                         <h4>1</h4>
@@ -17,7 +19,7 @@ export const BannerDeal = () => (
                 </div>
             </label>
             <input type="radio" name="slide" id="c2" />
-            <label for="c2" className="cardBanner">
+            <label for="c2" className="cardBanner card-effect" style={{ backgroundImage: `url(${bn1})` }}>
                 <div className="row">
                     <div className="icon">2</div>
                     <div className="description">
@@ -27,7 +29,7 @@ export const BannerDeal = () => (
                 </div>
             </label>
             <input type="radio" name="slide" id="c3" />
-            <label for="c3" className="cardBanner">
+            <label for="c3" className="cardBanner card-effect" style={{ backgroundImage: `url(${bn1})` }}>
                 <div className="row">
                     <div className="icon">3</div>
                     <div className="description">
@@ -36,8 +38,8 @@ export const BannerDeal = () => (
                     </div>
                 </div>
             </label>
-            <input type="radio" name="slide" id="c4" />
-            <label for="c4" className="cardBanner">
+            <input type="radio" name="slide" id="c4"/>
+            <label for="c4" className="cardBanner card-effect" style={{ backgroundImage: `url(${bn1})` }}>
                 <div className="row">
                     <div className="icon">4</div>
                     <div className="description">
--- src/front/js/component/navbar/navigationOffcanvas.js
+++ src/front/js/component/navbar/navigationOffcanvas.js
@@ -9,7 +9,6 @@ export const NavigationOffcanvas = () => {
   const { store, actions } = useContext(Context);
 
   return (
-<<<<<<< HEAD
     <div
       className="offcanvas offcanvas-start"
       tabIndex="-1"
@@ -31,57 +30,6 @@ export const NavigationOffcanvas = () => {
           >
             <div className="mb-2" data-bs-dismiss="offcanvas">
               <h6 className="text-effect-white border-0">My profile</h6>
-=======
-        <div
-          className="offcanvas offcanvas-start"
-          tabIndex="-1"
-          id="menuOffcanvas"
-          aria-labelledby="menuOffcanvasLabel"
-          style={{ width: "15%", background: "#020D19" }}
-        >
-          {/*---------------------------------------MENU CONTENTS---------------------------------*/}
-          <div className="offcanvas-header">
-            <h3 className="offcanvas-title text-white" id="menuOffcanvasLabel">
-              {!store.loggedIn ? ("Navigation") : (`Hi, ${store.user.username}`)}
-              
-            </h3>
-          </div>
-          <div className="offcanvas-body">
-            <div className="d-flex flex-column ms-4 my-3">
-              {store.loggedIn && store.user && (
-                <div>
-                  <Link
-                    className="text-white text-decoration-none mb-2"
-                    to={`/myprofile/${store.user.username}`}
-                  >
-                    <div className="mb-2" data-bs-dismiss="offcanvas">
-                      <h6 className="text-effect-white border-0">My profile</h6>
-                    </div>
-                  </Link>
-                  <Link
-                    className="text-white text-decoration-none mb-2"
-                    to={`/postdeal/user/${store.user.username}`}
-                  >
-                    <div className="mb-2" data-bs-dismiss="offcanvas">
-                      <h6 className="text-effect-white border-0">Post deal</h6>
-                    </div>
-                  </Link>{" "}
-                </div>
-              )}
-              <Link
-                className="text-white text-decoration-none mb-2"
-                to={"/games"}
-              >
-                <div data-bs-dismiss="offcanvas">
-                  <h6 className="text-effect-white border-0">Games</h6>
-                </div>
-              </Link>
-              <Link className="text-white text-decoration-none mb-2" to={"/"}>
-                <div className="mb-2" data-bs-dismiss="offcanvas">
-                  <h6 className="text-effect-white border-0">Deals</h6>
-                </div>
-              </Link>
->>>>>>> e3a9f5c11c8c5ee51e10a36c742ff058ebf08b20
             </div>
           </Link>
           {/*--------------------------------------post deal---------------------------------*/}
