5a066bfe26e3b85848e12b504c23dbc67d50ebc4
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2024-05-13 20:16:50 +0000

    bannerDeal

---
+++ src/front/js/component/deal/bannerDeal.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+
+
+export const BannerDeal = () => (
+
+<div className="wrapper">
+        <div className="containerBanner">
+            <input type="radio" name="slide" id="c1" checked />
+            <label for="c1" className="cardBanner">
+                <div class="row">
+                    <div className="icon">1</div>
+                    <div className="description">
+                        <h4>1</h4>
+                        <p>lorem</p>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c2" />
+            <label for="c2" className="cardBanner">
+                <div className="row">
+                    <div className="icon">2</div>
+                    <div className="description">
+                        <h4>2</h4>
+                        <p>Lorem</p>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c3" />
+            <label for="c3" className="cardBanner">
+                <div className="row">
+                    <div className="icon">3</div>
+                    <div className="description">
+                        <h4>3</h4>
+                        <p>lorem</p>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c4" />
+            <label for="c4" className="cardBanner">
+                <div className="row">
+                    <div className="icon">4</div>
+                    <div className="description">
+                        <h4>4</h4>
+                        <p>lorem</p>
+                    </div>
+                </div>
+            </label>
+        </div>
+        </div>
+        );
--- src/front/js/component/utils/footer.js
+++ src/front/js/component/utils/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => (
   <footer
     className="footer pt-3 bg-gradient"
-    style={{ background: "#020D19", position: "fixed", bottom: 0, width: "100%" }}
+    style={{ background: "#020D19", bottom: 0, width: "100%"}}
   >
     <p className= "text-center pb-3 text-white">
       Made by DKA <i className="fa-solid fa-skull-crossbones"></i>
